c315f5caf8301257fdd133abb8eb0cdf5fce6fef
Kendal <kray85@gmail.com>
2022-03-30 08:23:58 -0400

    added project keys

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client'
 import imageURLBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
+  projectId: "41kprfol",
   dataset: 'production',
   apiVerison: '2022-03-01',
   useCdn: 'true',
-  token: process.env.REACT_APP__SANITY_PROJECT_TOKEN,
+  token: "skeFR8wwJYRV3QdLMwLb9cFgieuXV7ufdgjbajHdfYo47o7WFCHvEdcNnSutDr0nvBuNPrNBxbAi2TOudVK2xpvl3WShExB9zPqRhxjt5z7ek",
 })
 
 const builder = imageURLBuilder(client)
