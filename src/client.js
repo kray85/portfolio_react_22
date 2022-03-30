924d09482acfff7222e67d0767fcaec788da5088
Kendal <kray85@gmail.com>
2022-03-29 22:05:57 -0400

    connected to sanity studio

---
+++ src/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client'
+import imageURLBuilder from '@sanity/image-url'
+
+export const client = sanityClient({
+    projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
+    dataset:'production',
+    apiVerison:'2022-03-01',
+    useCdn:'true',
+    token: process.env.REACT_APP__SANITY_PROJECT_TOKEN,
+})
+
+const builder = imageURLBuilder(client)
+
+export const urlFor = (source) => builder.image(source)
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,17 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 
-import { images } from '../../constants';
 import './About.scss'
-
-const abouts = [
-  {title: 'Web Developer', description: 'I am a good web developer', imgURL:images.about01},
-  {title: 'Web Design', description: 'I am a good web developer', imgURL:images.about02},
-  {title: 'Full-stack Web Developer', description: 'I am a good web developer', imgURL:images.about03},
-  {title: 'Software Engineer - Dev-Ops', description: 'I am a good web developer', imgURL:images.about04},
-];
+import { urlFor, client } from '../../client'
 
 const About = () => {
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
   return (
     <>
       <h2 className='head-text'>
@@ -21,23 +23,27 @@ const About = () => {
         means
         <span>Good Business</span>
       </h2>
-      <div className="app__profiles">
+      <div className='app__profiles'>
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
             className='app__profile-item'
-            key={about.title = index}
+            key={(about.title = index)}
           >
-            <img src={about.imgURL} alt={about.title} />
-            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{ about.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
-        
       </div>
-    </>)
+    </>
+  )
 }
 
 export default About
