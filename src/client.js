1562aa2075ae7311e4a78e0c844dbb5cd70fe6a9
Kendal <kray85@gmail.com>
2022-03-30 22:20:16 -0400

    changed sanity variables

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client'
 import imageURLBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
+  projectId: process.env.SANITY_STUDIO_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVerison: '2022-02-01',
   useCdn: 'true',
-  token: process.env.REACT_APP__SANITY_TOKEN,
+  token: process.env.SANITY_STUDIO_SANITY_TOKEN,
 })
 
 const builder = imageURLBuilder(client)
